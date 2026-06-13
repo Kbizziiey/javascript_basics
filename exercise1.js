81e13b60a3332c4cd001f7d3d61436f83f2f399a
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2026-06-13 17:51:15 +0200

    first exercise

---
+++ exercise1.js
@@ -0,0 +1,21 @@
+name = "Karabo Makganyoga"
+isStudent = true
+adress = {city: "jozi", country: "South Africa"}
+hobbies = ["soccer", "Hiking"]
+undefinedVariable = undefined
+nullVariable = null
+
+console.log(name)
+console.log(typeof(name))
+
+console.log(adress)
+console.log(typeof(adress))
+
+console.log(hobbies)
+console.log(typeof(hobbies))
+
+console.log(undefinedVariable)
+console.log(typeof(undefinedVariable))
+
+console.log(nullVariable)
+console.log(typeof(nullVariable))
