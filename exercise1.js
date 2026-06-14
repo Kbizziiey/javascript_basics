e3139b4c9a4963db21d066adb146c90a81d0d660
Karabo Makganyoga <karabomakganyoga2@gmail.com>
2026-06-14 22:22:50 +0200

    Implement Exercise 1 covering JavaScript data types and typeof operator. - Implement Exercise 2 for birth year calculation (Browser & Node.js versions). - Fix ReferenceError by decoupling browser prompt/alert from Node.js runtime. - Add comprehensive README.md detailing execution steps for both environments.

--- exercise1.js
+++ exercise1.js
@@ -5,6 +5,7 @@ let hobbies = ["soccer", "coding"];
 let undefinedVariable = undefined;
 let nullVariable = null;
 
+
 console.log(name, "Type: ", typeof name);
 
 console.log(adress, "Type:", typeof adress);
